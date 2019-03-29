e993ec5293fca7334feb54c0a1a77681c0399132
Robbert Martens <Robbertmartens@gmail.com>
2019-03-29 16:02:59 +0100

    Added Header class Moved login method from HomePage to Header

--- Domain/Pages/HomePage.cs
+++ Domain/Pages/HomePage.cs
@@ -7,13 +7,6 @@ namespace Domain.Pages
 {
     public class HomePage : Page
     {
-        private IWebElement _loginButton => Driver.FindElement(By.XPath("//* [@class='login']"));
-
         public HomePage(IWebDriver driver) : base(driver) { }
-
-        public void ClickLogin()
-        {
-            _loginButton.Click();
-        }
     }
 }
